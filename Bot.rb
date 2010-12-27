a03771a3c77027b6230e2e2c8211d5cca4134807
lep <lep@mini>
2010-12-28 00:28:03 +0100

    Added various commands to IRC::Bot.

--- Bot.rb
+++ Bot.rb
@@ -5,13 +5,25 @@ require 'Connection'
 module IRC
 	class Bot
 		def initialize server, nick, opts={}
-			
+			@connection = IRC::Connection.new server, nick, opts
 		end
 
 		#def on(join|part|rename|msg|priv, &block)
-		#def join channel, password=nil
-		#def part channel
-		#def msg to
-		#def topic what
+		
+		def join channel, password=nil
+			@connection.send "JOIN #{channel} #{password}".strip
+		end
+		
+		def part channel
+			@connection.send "PART #{channel}"
+		end
+		
+		def msg to, what
+			@connection.send "PRIVMSG #{to} :#{what}"
+		end
+		
+		def topic what
+			@connection.send "TOPIC :#{what}"
+		end
 	end
 end
