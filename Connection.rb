6dde2cf7c72546aa272f9fd149541b8b7f1c370a
lep <lep@mini>
2010-12-27 22:00:33 +0100

    IRC::Connection started. Can connect and listen.

---
+++ Connection.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+module IRC
+	class Connection
+		def initialize server, nick, opts={}
+			opts[:port] ||= 6667
+			opts[:server] = server
+			opts[:nick] = nick
+			@option=opts
+
+			connect
+			listen
+		end
+
+		private
+
+		def connect
+			socket = TCPSocket.open(@config[:server], @config[:port])
+
+			if @config.ssl
+				begin require 'openssl' 
+				rescue raise "Cannot load openssl-library" 
+				end
+
+				context = OpenSSL::SSL::SSLContext.new
+				context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+				
+				socket = OpenSSL::SSL::SSLSocket.new(socket, context)
+				socket.sync = true
+				socket.connect
+			end
+
+			@socket = socket
+		end
+
+		def listen
+			Thread.new do
+				while line = @socket.gets
+					parse line
+				end
+			end
+		end
+
+		def parse line
+			match = /(?:^:(\S+) )(\S+) (.+)/.match line
+		end
+	end
+end
