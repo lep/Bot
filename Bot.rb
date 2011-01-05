ce0b05e504cab93d51f8a290764fd3ffe3e46d02
lep <lep@mini>
2011-01-05 19:30:31 +0100

    Rewrote internal Bot-dispatcher.

--- Bot.rb
+++ Bot.rb
@@ -6,11 +6,25 @@ module IRC
 
 	class Bot
 	  @@valid_types = [:join, :part, :rename, :msg, :priv, :quit]
+		@@simple_commands = %w{JOIN PART QUIT NICK QUIT}
+		@@command_to_symbol = {}
 
 		def initialize server, nick, opts={}
 			@connection = IRC::Connection.new server, nick, opts
-			%w{JOIN PART PRIVMSG QUIT}.each do |command|
-				@connection.on command, do |params, prefix|
+
+			#table for simple commands with a specifi pattern
+			#callback(prefix, params[0])
+			@@simple_commands.each do |command|
+				@@command_to_symbol[command]=command.downcase.to_sym
+			end
+
+			#for me, this is not the priciple of lest surprise
+			#i mean that concant of an array for one element
+			#i'd wish for something like (ary + element).each
+
+			#dispatches every command
+			@@simple_commands.concat(["PRIVMSG"]).each do |command|
+				@connection.on command do |params, prefix|
 					dispatch command, params, prefix
 				end
 			end
@@ -19,20 +33,19 @@ module IRC
 
 		#def on(join|part|rename|msg|priv|quit, &block)
 		=begin example
-			def on :msg, :channel, /^!(.*)$/ do |user, msg, channel, match_data|
-				
+			def on :msg, :channel, /^!(.*)$/ do |user, msg, match|
 			end
 
 			def on :msg, "#gp39f.tmp" do |user, msg|
-
 			end
 
 			def on :msg, :rant do |user, msg|
-
 			end
 
-			def on 
+			def on :msg, /\d{32}/ do |user, msg, channel, match|
+			end
 		=end
+
 		def on type, channel=nil, regexp=nil, &callback
 			unless @@valid_types.contains? type
 				raise "Type (#{type}) not known."
@@ -62,7 +75,12 @@ module IRC
 			else
 			end
 
-			@callbacks[action] << callback
+			#TODO make own class (for PRIVMSG)?
+			@callbacks[action] <<
+				{ :callback => callback
+				, :regexp => regexp
+				, :channel => channel
+				}
 		end
 		
 		def join channel, password=nil
@@ -87,25 +105,16 @@ module IRC
 
 
 		def dispatch command, params, prefix
-			case command
-			when "JOIN" #|user, channel|
-				@callbacks[:join].each do |cb|
+			if @@simple_commands.contains? command
+				@callbacks[@@command_to_symbol[command]].each do |cb|
 					cb.call prefix, params[0]
 				end
-			when "PART" #|user, channel|
-				@callbacks[:part].each do |cb|
-					cb.call prefix, params[0]
+			else
+				unless command == "PRIVMSG"
+					raise "Unknown Command."
 				end
-			when "NICK" #|user, new_name|
-				@callbacks[:nick].each do |cb|
-					cb.call prefix, *params
-				end
-			when "QUIT" #|user, quit_msg|
-				@callbacks[:quit].each do |cb|
-					cb.call prefix, params[0]
-				end
-			when "PRIVMSG" #|user,(channel,) msg|
-				if params[0].chr =="#"
+
+				if params[0, 1] =="#"
 					@callbacks[:msg].each do |cb|
 						cb.call prefix, params[0], params[1]
 					end
