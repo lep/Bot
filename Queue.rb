78ec166e269e012bcdef499864bd0c9fc59b484a
lep <lep@next.(none)>
2011-01-08 20:55:11 +0100

    Added IRC::Queue.

---
+++ Queue.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+module IRC
+
+	class Queue
+		def initialize
+			@locked = false
+			@queue = []
+		end
+
+		def << *params
+			@queue << *params
+		end
+
+		def lock
+			@locked = true
+		end
+
+		def unlock
+			@locked = false
+		end
+
+		def locked?
+			@locked
+		end
+
+		def empty?
+			@queue.empty?
+		end
+
+		def first
+			@queue.first
+		end
+
+		def remove
+			@queue.shift
+		end
+	end
+end
